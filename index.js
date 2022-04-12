07fabfc66ca5a182bbe9b43f26b1a13dbb53a7b9
Josef Reichelt <pepareichelt@gmail.com>
2022-04-12 20:32:33 +0200

    Add random colors

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const logo = document.getElementById('dvdlogo');
 const container = document.getElementById('drive');
 
+const speed = 2;
 let width = container.clientWidth;
 let height = container.clientHeight;
-const speed = 5;
 let velocityX = speed;
 let velocityY = speed;
 let frameId = 0;
 
-function update(){
-    if(!logo) {
+function update() {
+    if (!logo) {
         cancelAnimationFrame(frameId)
     }
+
     const logoBox = logo.getBoundingClientRect()
-    if(logoBox.x + logoBox.width + speed >= width){
+
+    if (logoBox.x + logoBox.width + speed >= width) {
         velocityX = -speed;
-    }else if(logoBox.x - speed <= 0){
+        pickColor(logo);
+    } else if (logoBox.x - speed <= 0) {
         velocityX = speed;
+        pickColor(logo);
     }
-    if(logoBox.y + logoBox.height + speed >= height){
+    if (logoBox.y + logoBox.height + speed >= height) {
         velocityY = -speed;
-    }else if(logoBox.y - speed <= 0){
+        pickColor(logo);
+    } else if (logoBox.y - speed <= 0) {
         velocityY = speed;
+        pickColor(logo);
     }
 
     logo.style.left = `${logoBox.x + velocityX}px`;
@@ -30,13 +36,22 @@ function update(){
     frameId = requestAnimationFrame(update)
 }
 
-
-function handleResize(e){
+function handleResize() {
     width = container.clientWidth;
     height = container.clientHeight;
 }
 
+function pickColor(logoElement) {
+    const randomHue = Math.random() * 360;
+    logoElement.style.fill = `hsl(${randomHue},85%,60%)`;
+}
 
+function setRandomPosition() {
+    logo.style.left = `${Math.max(0, Math.min(Math.random() * width - logo.clientWidth, width))}px`;
+    logo.style.top = `${Math.max(0, Math.min(Math.random() * height - logo.clientHeight, height))}px`;
+}
 
-window.addEventListener('resize',handleResize);
+pickColor(logo);
+setRandomPosition();
+window.addEventListener('resize', handleResize);
 frameId = requestAnimationFrame(update)
